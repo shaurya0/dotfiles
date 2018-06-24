bc866020b18653c607cfb3e24fecbffa1530b7c1
Shaurya Sharma <shaur141@gmail.com>
2018-06-24 19:04:09 +0200

    bindkeys

---
+++ .bindkeys.zsh
@@ -0,0 +1,19 @@
+# ctrl+b/f or ctrl+left/right : move word by word (backward/forward)
+bindkey '^[j' backward-word
+bindkey '^[l' forward-word
+
+bindkey '^[k' backward-delete-char
+bindkey '^h' delete-char-or-list
+bindkey '^kk' kill-line
+
+bindkey "^[u" backward-char
+bindkey "^[o" forward-char
+
+bindkey "^[n" beginning-of-line
+bindkey "^[m" end-of-line
+
+bindkey "^[K" backward-kill-word
+bindkey "^H" kill-word
+
+
+
