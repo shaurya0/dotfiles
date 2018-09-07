b1ffacc5e93f0d882a88199f4f6de073924f6c4a
Shaurya Sharma <shaurya@something.com>
2018-09-07 22:47:29 +0200

    wip

--- install.sh
+++ install.sh
@@ -1,6 +1,21 @@
+sudo apt-get install zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+mkdir -p temp
+git clone https://gist.github.com/shaurya0/d22dd8d500a8e55adf444c9fe32cd99a temp
+mv temp/agnoster-newline.zsh-theme ~/.oh-my-zsh/themes/
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+ ~/.fzf/install
+
+wget https://github.com/sharkdp/bat/releases/download/v0.6.1/bat_0.6.1_amd64.deb -O temp/bat.deb
+sudo dpkg -i temp/bat.deb
+
+rm -rf temp
+
 DOTFILES="$(find . -name ".*" -type f)"
 for f in $DOTFILES; do
-	mv $f ~/
+    cp $f ~/
 done
 
 
