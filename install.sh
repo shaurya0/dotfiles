d04e82e639acb470832d8504586ef6f789af9b52
Shaurya Sharma <shaurya@something.com>
2018-09-08 12:46:19 +0200

    wip

--- install.sh
+++ install.sh
@@ -1,15 +1,34 @@
-sudo apt-get install zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
 mkdir -p temp
-git clone https://gist.github.com/shaurya0/d22dd8d500a8e55adf444c9fe32cd99a temp
-mv temp/agnoster-newline.zsh-theme ~/.oh-my-zsh/themes/
+if ! [ -x "$(command -v zsh)" ]; then
+    sudo apt-get install zsh
+    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
- ~/.fzf/install
+    git clone https://gist.github.com/shaurya0/d22dd8d500a8e55adf444c9fe32cd99a temp
+    mv temp/agnoster-newline.zsh-theme ~/.oh-my-zsh/themes/
+else
+    echo "zsh already installed"
+fi
 
-wget https://github.com/sharkdp/bat/releases/download/v0.6.1/bat_0.6.1_amd64.deb -O temp/bat.deb
-sudo dpkg -i temp/bat.deb
+if [ ! -d ~/.fzf ]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+else
+    echo "fzf already installed"
+fi
+
+if ! [ -x "$(command -v bat)" ]; then
+    wget https://github.com/sharkdp/bat/releases/download/v0.6.1/bat_0.6.1_amd64.deb -O temp/bat.deb
+    sudo dpkg -i temp/bat.deb
+else
+    echo "bat already installed"
+fi
+
+if ! [ -x "$(command -v rg)" ]; then
+    wget https://github.com/BurntSushi/ripgrep/releases/download/0.9.0/ripgrep_0.9.0_amd64.deb -O temp/ripgrep.deb
+    sudo dpkg -i temp/ripgrep.deb
+else
+        echo "ripgrep already installed"
+fi
 
 rm -rf temp
 
