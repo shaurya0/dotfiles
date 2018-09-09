91f6ef5accc3638d9a2a29bf588589b448d2e0ad
Shaurya Sharma <shaur141@gmail.com>
2018-09-09 10:51:35 +0200

    wip

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@ if ! [ -x "$(command -v zsh)" ]; then
     sudo apt-get install zsh
     sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-    git clone https://gist.github.com/shaurya0/d22dd8d500a8e55adf444c9fe32cd99a temp
-    mv temp/agnoster-newline.zsh-theme ~/.oh-my-zsh/themes/
 else
     echo "zsh already installed"
 fi
+
+git clone https://gist.github.com/shaurya0/d22dd8d500a8e55adf444c9fe32cd99a temp
+mv temp/agnoster-newline.zsh-theme ~/.oh-my-zsh/themes/
+
 
 if [ ! -d ~/.fzf ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
